11ea65449492cf5cd5a07fd198d67ad0e6a85e8a
Tyler Peryea <peryeata@cezanne.ncats.nih.gov>
2015-04-09 21:28:31 -0400

    Initial commit

---
+++ button.js
@@ -0,0 +1,14 @@
+colorTimer = window.setInterval(function(){ 
+var s = parseInt($("#thebutton-s-10s").html()+$("#thebutton-s-1s").html());
+var ring = $('.thebutton-pie-container')
+ring.css('-webkit-transition', 'background-color 0.4s')
+    .css('-moz-transition', 'background-color 0.4s')
+    .css('-ms-transition', 'background-color 0.4s')
+    .css('-o-transition', 'background-color 0.4s')
+    .css('transition', 'background-color 0.4s');
+if (s < 12) { ring.css('background-color', '#e50000') } else
+if (s < 22) { ring.css('background-color', '#e59500') } else
+if (s < 32) { ring.css('background-color', '#e5d900') } else
+if (s < 42) { ring.css('background-color', '#02be01') } else
+if (s < 52) { ring.css('background-color', '#0083c7') } else
+{ ring.css('background-color', '#820080') }}, 10);
